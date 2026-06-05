0f23ad9af3523c8a4dba6aee7f22b869ca3fbf15
shrutig-blip <shrutigupta6002@gmail.com>
2026-06-06 00:45:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Technominds_HackBridge/',
+
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
